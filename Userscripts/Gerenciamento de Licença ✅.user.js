26e931760a2e62fdcc42f5724e250ca20527927f
João Paulo <65697347+p4ulinho@users.noreply.github.com>
2024-07-16 21:15:41 -0300

    Add files via upload

---
+++ Userscripts/Gerenciamento de Licença ✅.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Gerenciamento de Licença ✅
+// @description  Confira a data de expiração do seu pacote mensal e renove automaticamente.
+// @author       PaulinhoTutoriais
+// @include      **game*
+// @icon         https://www.dl.dropboxusercontent.com/scl/fi/93px3rbj21zx3863m0g40/letter-p.png?rlkey=yyoejfwogbl9rdw0p2fmft8lr&dl=0
+// @grant        none
+// ==/UserScript==
+
