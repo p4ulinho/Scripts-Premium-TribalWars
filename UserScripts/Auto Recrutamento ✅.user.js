0fe0749d405e4c9cfd5ef32a09e5e0843f12a788
João Paulo <65697347+p4ulinho@users.noreply.github.com>
2024-08-09 16:14:31 -0300

    Update Auto Recrutamento ✅.user.js

--- UserScripts/Auto Recrutamento ✅.user.js
+++ UserScripts/Auto Recrutamento ✅.user.js
@@ -3,6 +3,9 @@
 // @author         PaulinhoTutoriais
 // @include        **&screen=train*
 // @include        **screen=train&mode=train&village*
+// @include        **combined*
+// @exclude        **screen=train&mode=mass*
+// @exclude        **&screen=train&mode=success*
 // @icon           https://www.dl.dropboxusercontent.com/scl/fi/93px3rbj21zx3863m0g40/letter-p.png?rlkey=yyoejfwogbl9rdw0p2fmft8lr&dl=0
 // ==/UserScript==
 
